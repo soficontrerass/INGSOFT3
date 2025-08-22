376219e6dd81db7683435b361687ab5703946a60
sofi contreras <2215803@ucc.edu.ar>
2025-08-22 15:17:52 -0300

    feat: permite despedir por nombre

--- src/app.js
+++ src/app.js
@@ -1,10 +1,19 @@
 // Versión inicial
 function saludar(nombre) {
-    if (nombre) {
-        console.log(`Hola, ${nombre}`);
-    } else {
+    if (!nombre) {
         console.log("Hola mundo");
+    } else {
+        console.log(`Hola, ${nombre}`);
     }
 }
 
+// despedir por nombre
+function despedir(nombre) {
+    if (!nombre) {
+        console.log("Adiós mundo");
+    } else {
+        console.log(`Adiós, ${nombre}`);
+    }
+}
 saludar("Juan");
+despedir("Juan");
