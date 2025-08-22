ec7ac943dcc5696a930b6eba91bb042b7ed0a4d4
sofi contreras <2215803@ucc.edu.ar>
2025-08-22 15:21:06 -0300

    fix: arregla error en produccion

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 // Versión inicial
+<<<<<<< HEAD
 function saludar(nombre) {
     if (!nombre) {
         console.log("Hola mundo");
     } else {
         console.log(`Hola, ${nombre}`);
     }
+=======
+function saludar() {
+    console.log("Hola mundo");
+>>>>>>> f01557e (fix: arregla error en produccion)
 }
 
 // despedir por nombre
