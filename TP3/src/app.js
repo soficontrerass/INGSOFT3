9ae90a43bad3851532b7cc8f21e07a9d49348bf8
sofi contreras <2215803@ucc.edu.ar>
2025-09-23 15:06:04 -0300

    Agrega función de login

--- TP3/src/app.js
+++ TP3/src/app.js
@@ -1,1 +1,10 @@
 console.log("Sistema de login funcionando.");
+
+function login(email, password) {
+  // Simulación de login
+  if (email === "user@example.com" && password === "123456") {
+    return "Login exitoso";
+  } else {
+    return "Credenciales incorrectas";
+  }
+}
