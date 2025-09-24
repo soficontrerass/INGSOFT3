de20bd80cc09075ed89568b334e8b1c7982b0730
sofi contreras <2215803@ucc.edu.ar>
2025-09-24 12:35:03 -0300

    Agrega evidencias y ejemplo de uso en app.js

--- TP3/src/app.js
+++ TP3/src/app.js
@@ -29,4 +29,7 @@ function recoverPassword(email) {
   }
 }
 
-
+// Ejemplos de uso:
+console.log(login("user@example.com", "123456")); // Login exitoso
+console.log(register("nuevo@correo.com", "abcdef")); // Registro exitoso
+console.log(recoverPassword("user@example.com")); // Email de recuperación enviado
