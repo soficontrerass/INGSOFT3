01b1b170040e2a0fd2d94c45d3c4dfd7fa78c5ee
sofi contreras <2215803@ucc.edu.ar>
2025-09-25 16:30:30 -0300

    fix: resuelvo conflicto tras integración del fix en saludar

--- src/app.js
+++ src/app.js
@@ -1,15 +1,10 @@
 // Versión inicial
-<<<<<<< HEAD
 function saludar(nombre) {
     if (!nombre) {
         console.log("Hola mundo");
     } else {
         console.log(`Hola, ${nombre}`);
     }
-=======
-function saludar() {
-    console.log("Hola mundo");
->>>>>>> f01557e (fix: arregla error en produccion)
 }
 
 // despedir por nombre
@@ -20,5 +15,6 @@ function despedir(nombre) {
         console.log(`Adiós, ${nombre}`);
     }
 }
+
 saludar("Juan");
 despedir("Juan");
